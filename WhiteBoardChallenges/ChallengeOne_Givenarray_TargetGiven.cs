5c89110b13ee2375085daebc13b5c9f0da699f9d
unknown <ajpbarry@gmail.com>
2020-01-29 09:36:04 -0600

    init

---
+++ WhiteBoardChallenges/ChallengeOne_Givenarray_TargetGiven.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteBoardChallenges
+{
+    class ChallengeOne_Givenarray_TargetGiven
+    {
+
+        //Member Variables (HAS A)
+        int[] addendOne = new int[] { 5, 17, 77, 50 };
+        int[] addendTwo = new int[] { 5, 17, 77, 50 };
+        int target = 55;
+        //Constructor
+        ChallengeOne_Givenarray_TargetGiven()
+        {
+
+        }
+
+        //Member Methods (CAN DO)
+        public int RunChallengeOne()
+        {
+            for (int i = 0; i < addendOne.Length; i++)
+            {
+                for (int j = 0; j < addendTwo.Length; j++)
+                {
+                    int result = addendOne[i] + addendOne[j];
+                    if ( i == j)
+                    {
+                        continue;
+                    }  
+                    else if (result == target)
+                    {
+                        return addendOne[i] + addendTwo[j];
+                    }
+
+                }
+            }
+             throw new Exception("Given numbers do not add up to target.");
+        } 
+    }
+}
